d2bef5558e1dc27b029874d316749f05cdce73f8
vinaykumargt <120788680+vinaykumargt@users.noreply.github.com>
2023-02-20 22:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const yearInput = document.querySelector('#year');
+const button = document.querySelector('#button');
+const url = document.querySelector('#url');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (nameInput.value.trim() === '') {
+    alert('Please enter name');
+    return;
+  }
+  const params = new URLSearchParams({
+    name: nameInput.value,
+    year: yearInput.value
+  });
+  const queryString = params.toString();
+  url.textContent = `https://localhost:8080/?${queryString}`;
+  form.reset();
+});
+
